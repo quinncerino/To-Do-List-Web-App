1f2e8d53a06003112e97aad0aafe576361aeb812
quinncerino <quinncerino@gmail.com>
2025-07-14 23:23:03 -0400

    changed checkbox keys

--- web.py
+++ web.py
@@ -19,7 +19,7 @@ st.write("This app's purpose is to increase your <b>productivity</b>.", unsafe_a
 
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    checkbox = st.checkbox(todo, key=f"todo_{index}")
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
